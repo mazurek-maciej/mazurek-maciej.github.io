52141a8b7dd0ec95f355807e83f2e72f3b74cc9f
Maciej Mazurek <maciej.mazurek.prv@gmail.com>
2018-08-08 15:37:41 +0200

    Dokończenie wszystkich sekcji
    - utworzenie sektorów usług z działającym js-em (potrzeba by zmienić na j-query)
    - utworzenie sekcji kontakt
    - prosta stopka

--- kancelaria/resources/js/script.js
+++ kancelaria/resources/js/script.js
@@ -1,30 +1,18 @@
 function openService1(evt, service) {
   var i, x, tablinks;
-  x = document.getElementsByClassName("service1");
+  x = document.getElementsByClassName("service");
   for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";
   }
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+      tablinks[i].className = tablinks[i].className.replace(" all-services-btn-active", "");
   }
   document.getElementById(service).style.display = "block";
-  evt.currentTarget.className += " w3-red";
+  evt.currentTarget.className += " all-services-btn-active";
 }
 
-function openService2(evt, service) {
-  var i, x, tablinks;
-  x = document.getElementsByClassName("service2");
-  for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
-  }
-  document.getElementById(service).style.display = "block";
-  evt.currentTarget.className += " w3-red";
-}
+
 
 
 
@@ -40,6 +28,8 @@ $(document).ready(function() {
     }, {
         offset: '60px;'
     });
+    
+    
     
     /*Scroll buttons */
     $(function() {
